d1331457eaceff37778af38afda06c54d44ab3ad
Phil Austin <paustin@eos.ubc.ca>
2021-10-13 09:06:27 -0700

    cherry-pick lab6

---
+++ copy_node08.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -v
+scp -r n8comp:/home/phil/repos/e211hub/datadir/e211_files/exchange/e211/outbound/* outbound_hold/.
+scp -r n8comp:/home/phil/repos/e211hub/datadir/e211_files/source/* source_hold/.
+
