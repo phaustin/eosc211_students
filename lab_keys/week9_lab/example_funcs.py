07f6cd884e970fd6d3f765eae677d6183e96bb99
Phil Austin <you@example.comw>
2021-11-09 10:24:19 -0800

    lab9 key

---
+++ lab_keys/week9_lab/example_funcs.py
@@ -0,0 +1,19 @@
+def least_probable():
+    """
+    returns the least probable outcome, given that one is being pursued
+    by a pair of guided nuclear missiles 
+    
+    (If you have not read the hitchhikers guide to the galaxy, consider 
+     it to be on the required readings list for next week.)
+    """
+    return "whale", "petunias"
+
+def earth():
+    """
+    returns a brief description of the planet known as earth
+    """
+    return "Harmless"
+def another_fcn():
+    return "halelujah"
+def another_fcn():
+    return "halelujah"
